14d4b1ee57604d0b21232d5eeb39247572dcf952
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-20 15:37:55 +0100

    buttons connected

--- src/App.js
+++ src/App.js
@@ -3,42 +3,94 @@ import TextField from 'material-ui/TextField'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import {List, ListItem} from 'material-ui/List';
+import ActionDelete from 'material-ui/svg-icons/action/delete'
+
 
 const style = {
-
     margin: 20,
     textAlign: 'center',
-
 };
 
+const Task = (props) => {
+    return <ListItem
+        primaryText={props.taskName}
+        rightIcon={
+            <ActionDelete
+                onClick={() => props.deleteTask(props.taskId)}
+            />}
+            leftIcon={
+                <ActionInfo
+            onClick={() => props.crossTask(props.taskId)}
+            />
+        }
+
+    />
+}
+
+
 class App extends Component {
+
+    addTask = () => {
+        alert('ADD TASK')
+    }
+
+    deleteTask = () => {
+        alert('DELETE TASK')
+    }
+    crossTask = () => {
+        alert('CROSS TASK')
+    }
+
     render() {
         return (
             <MuiThemeProvider>
-            <div>
-                <Paper style={style} zDepth={5}>
-                    <h1>React ToDo List</h1>
-                <TextField
-                    text
-                    hintText="Type here"
-                    floatingLabelText="Please type your task"
-                    fullWidth={true}
-                /><br />
-                </Paper>
                 <Paper style={style} zDepth={3}>
-                <RaisedButton
-                    label="ADD TASK"
-                    fullWidth={true}
-                    primary={true}
-                />
+                    <h1>React ToDo List</h1>
 
+
+                    <TextField
+                        hintText={"Type here"}
+                        floatingLabelText={"Please type your task"}
+                        fullWidth={true}
+                    />
+
+
+                    <RaisedButton
+                        label={"ADD TASK"}
+                        fullWidth={true}
+                        primary={true}
+                        onClick={() => {
+                            this.addTask()
+                        }}
+                    />
+
+                    <List>
+                        <Task
+                            taskName={'Pierwszy task'}
+                            taskId={0}
+                            deleteTask={() => {
+                                this.deleteTask()
+                            }}
+                            crossTask={()=> {
+                                this.crossTask()
+                            }}
+                        />
+                        <Task
+                            taskName={'drugi task'}
+                            taskId={0}
+                            deleteTask={() => {
+                                this.deleteTask()
+                            }}
+                            crossTask={()=> {
+                                this.crossTask()
+                            }}
+                        />
+                    </List>
                 </Paper>
-
-            </div>
             </MuiThemeProvider>
         )
-
-
     }
 }
 
